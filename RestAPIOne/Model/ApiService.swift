ab218f1af3d49eba6969b5ab3074ce9b1f500e9b
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-10-24 17:01:41 +0300

    Project complete
    Update UI cell

--- RestAPIOne/Model/ApiService.swift
+++ RestAPIOne/Model/ApiService.swift
@@ -31,7 +31,6 @@ class ApiService {
                 
                 DispatchQueue.main.async {
                     completion(.success(jsonData))
-                    print(jsonData)
                 }
                 
                 
